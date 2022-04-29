fbaf612868e5afe012aa4babc7ef48156a7943b6
Luis Brienze <luis.brienze@imaginelearning.com>
2022-04-29 12:27:09 -0700

    configuring topic and queue correct names

--- init-scripts/02-resources-creation.sh
+++ init-scripts/02-resources-creation.sh
@@ -3,10 +3,10 @@
 echo "-------------------------------------Script-02"
 
 echo "########### Creating SQS ###########"
-awslocal sqs create-queue --queue-name testQueue --profile=localstack
+awslocal sqs create-queue --queue-name cryptoAnalysisQueue --profile=localstack
 
 echo "########### Creating SNS ###########"
-awslocal sns create-topic --name testTopic --profile=localstack
+awslocal sns create-topic --name cryptoAnalysisTopic --profile=localstack
 
 echo "########### Listing SQS ###########"
 awslocal sqs list-queues --profile=localstack
@@ -15,7 +15,7 @@ echo "########### Listing SNS ###########"
 awslocal sns list-topics --profile=localstack
 
 echo "########### Subscribing SQS to SNS ###########"
-awslocal sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:testTopic --protocol sqs --notification-endpoint "http://localhost:4566/000000000000/testQueue" --profile=localstack
+awslocal sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:cryptoAnalysisTopic --protocol sqs --notification-endpoint "http://localhost:4566/000000000000/cryptoAnalysisQueue" --profile=localstack
 
 echo "########### Listing SNS Subscriptions ###########"
 awslocal sns list-subscriptions --profile=localstack

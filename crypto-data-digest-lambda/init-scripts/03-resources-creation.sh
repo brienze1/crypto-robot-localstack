b35f58d8e8b4013879d8e6f136f2331b74b8615c
Luis Brienze <lfbrienze@gmail.com>
2022-07-20 11:32:05 -0300

    changed docker-compose to work directly with awscli image from aws

--- crypto-data-digest-lambda/init-scripts/03-resources-creation.sh
+++ crypto-data-digest-lambda/init-scripts/03-resources-creation.sh
@@ -3,7 +3,7 @@
 echo "-----------------Script-02----------------- [data-digest]"
 
 echo "########### Copy the lambda function to the S3 bucket ###########"
-aws s3 cp crypto-robot-data-digest.zip s3://lambda-functions --endpoint-url http://localstack:4566
+aws s3 cp /lambda-files/crypto-robot-data-digest.zip s3://lambda-functions --endpoint-url http://localstack:4566
 
 echo "########### Create the lambda dataDigestLambda ###########"
 aws lambda create-function \
